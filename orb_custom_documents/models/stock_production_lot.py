8ad8bde471be03d5bf26b496d85b3182f0e9c6c6
javierjcf <javierjcf@gmail.com>
2020-04-20 21:30:29 +0200

    [FIX]orb_custom_documents: Algoritmo scar digito control ean13 del ean14 (barcode de producto)

--- orb_custom_documents/models/stock_production_lot.py
+++ orb_custom_documents/models/stock_production_lot.py
@@ -9,18 +9,22 @@ class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
     label_barcode = fields.Char(string='Label Barcode', compute='_get_barcode')
-    force_control = fields.Integer(string='Force Control Digit')
+    label_ean13 = fields.Char(string='Label EAN13', compute='_get_barcode')
 
     @api.model
     def get_control_digit(self, ean13):
+        """
+        Obtiene el código de control a partir de los 12 dígitos del ean13
+        """
         res = '1'
-        num = 1
+        idx = 1
         sum_digits = 0
         for digit in ean13:
             num = int(digit)
-            if num % 2 == 0:  # par position
+            if idx % 2 == 0:  # par position
                 num = num * 3
             sum_digits += num
+            idx += 1
 
         nearest_10_mult = math.ceil(sum_digits / 10.0) * 10
         res = nearest_10_mult - sum_digits
@@ -28,17 +32,23 @@ class StockProductionLot(models.Model):
 
 
     def _get_barcode(self):
-       for lot in self:
+        """
+        El barcode debe ser el EAN14, el ean13 lo calculamos a partir de este y
+        el código de barras lo compongo con la info del ean13
+        """
+        for lot in self:
             res = ''
-            if lot.product_id.barcode and len(lot.product_id.barcode) == 13:
-                ean13 = lot.product_id.barcode
+            ean13 = ''
+            if lot.product_id.barcode and len(lot.product_id.barcode) == 14:
+                ean14 = lot.product_id.barcode
                 code_type = '(01)'
                 digit = '1'
+                ean13 = ean14[1:-1]  # EAN13 sin dígito control (12 sígitos)
                 ean13_1 = ean13[:7]
-                ean13_2 = ean13[7:-1]
+                ean13_2 = ean13[7:]
+                control = ean14[-1]
+                ean13_control = self.get_control_digit(ean13)
                 batch_type = '(10)'
-                control = lot.force_control
-                if not control:
-                    control = self.get_control_digit(ean13)
                 res = ' '.join([code_type, digit, ean13_1, ean13_2, control, batch_type, lot.name])
             lot.label_barcode = res
+            lot.label_ean13 = ean13 + ean13_control
