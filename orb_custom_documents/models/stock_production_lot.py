dec19f919ba5e1db873606d59fb732c37244cd87
javierjcf <javierjcf@gmail.com>
2020-04-20 15:03:53 +0200

    [ADD]cost_sheets_lupeon: Calculo ean 14

--- orb_custom_documents/models/stock_production_lot.py
+++ orb_custom_documents/models/stock_production_lot.py
@@ -3,12 +3,28 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models
-
+import math
 
 class StockProductionLot(models.Model):
     _inherit = 'stock.production.lot'
 
-    label_barcode = fields.Char(string='Label Uom', compute='_get_barcode')
+    label_barcode = fields.Char(string='Label Barcode', compute='_get_barcode')
+
+    @api.model
+    def get_control_digit(self, ean13):
+        res = '1'
+        num = 1
+        sum_digits = 0
+        for digit in ean13:
+            num = int(digit)
+            if num % 2 == 0:  # par position
+                num = num * 3
+            sum_digits += num
+
+        nearest_10_mult = math.ceil(sum_digits / 10.0) * 10
+        res = nearest_10_mult - sum_digits
+        return str(res)
+
 
     def _get_barcode(self):
        for lot in self:
@@ -18,8 +34,8 @@ class StockProductionLot(models.Model):
                 code_type = '(01)'
                 digit = '1'
                 ean13_1 = ean13[:7]
-                ean13_2 = ean13[8:]
+                ean13_2 = ean13[7:-1]
                 batch_type = '(10)'
-                control = '1'
+                control = self.get_control_digit(ean13)
                 res = ' '.join([code_type, digit, ean13_1, ean13_2, control, batch_type, lot.name])
             lot.label_barcode = res
